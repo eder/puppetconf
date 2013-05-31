d4a9ec8318ff4153ac26f55bb90124824ffb5b7f
edereduardo <eder.esilva@gmail.com>
2013-05-31 19:50:22 -0300

    Added conf default of Nginx

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,3 +6,18 @@ package {
   "nginx":
      ensure => present
 }
+
+service {
+  "nginx":
+    ensure => true,
+    enable => true
+}
+
+file {
+  "/etc/nginx/sites-enabled/default" :
+  source => "puppet:///modules/nginx/default",
+  mode   => 644,
+  owner  => root,
+  group  => root
+
+}
